b785b52698f4d7e3ebfc32f30d3253ba32cfc5ba
lzlliuzheliang <lzlliuzheliang@gmail.com>
2019-01-30 10:11:11 -0500

    add sqlalchemy

--- src/initdatabase/app.py
+++ src/initdatabase/app.py
@@ -12,28 +12,40 @@ mydb = database.Mydatabase()
 # to_do 
 # if database is empty set a begin_timestamp, otherwise use the max_timestamp as begin_timestamp
 begin_timestamp = database.get_max_timestamp(mydb.DBSession)+1
+
+first_run = False
 if begin_timestamp == 0:
+	first_run = True
 	begin_timestamp = int(datetime.datetime(2009, 1, 1).timestamp())
 
-print("Begin timestamp: "begin_timestamp)
+print("Begin timestamp: " + begin_timestamp)
 
-# btc_data = mc.get_all_data_types_for_asset('btc', begin_timestamp, end_timestamp)
 btc_data = mc.get_all_data_types_for_asset('btc', begin_timestamp)
-database.core_bulk_insert_data('btc', mydb.engine, btc_data)
-
-# bch_data = mc.get_all_data_types_for_asset('bch', begin_timestamp, end_timestamp)
 bch_data = mc.get_all_data_types_for_asset('bch', begin_timestamp)
-database.core_bulk_insert_data('bch', mydb.engine, bch_data)
-
-# ltc_data = mc.get_all_data_types_for_asset('ltc', begin_timestamp, end_timestamp)
 ltc_data = mc.get_all_data_types_for_asset('ltc', begin_timestamp)
-database.core_bulk_insert_data('ltc', mydb.engine, ltc_data)
+eth_data = mc.get_all_data_types_for_asset('eth', begin_timestamp)
+etc_data = mc.get_all_data_types_for_asset('etc', begin_timestamp)
+
+if first_run:
+	database.core_bulk_insert_data('btc', mydb.engine, btc_data)
+	database.core_bulk_insert_data('bch', mydb.engine, bch_data)
+	database.core_bulk_insert_data('ltc', mydb.engine, ltc_data)
+	database.core_bulk_insert_data('eth', mydb.engine, eth_data)
+	database.core_bulk_insert_data('etc', mydb.engine, etc_data)
+else:
+	database.core_single_insert_data('btc', mydb.engine, btc_data)
+	database.core_single_insert_data('bch', mydb.engine, bch_data)
+	database.core_single_insert_data('ltc', mydb.engine, ltc_data)
+	database.core_single_insert_data('eth', mydb.engine, eth_data)
+	database.core_single_insert_data('etc', mydb.engine, etc_data)
 
 # eth_data = mc.get_all_data_types_for_asset('eth', begin_timestamp, end_timestamp)
-eth_data = mc.get_all_data_types_for_asset('eth', begin_timestamp)
-database.core_bulk_insert_data('eth', mydb.engine, eth_data)
+
+
 
 # etc_data = mc.get_all_data_types_for_asset('etc', begin_timestamp, end_timestamp)
-etc_data = mc.get_all_data_types_for_asset('etc', begin_timestamp)
+
 # for data in etc_data.values():
-database.core_bulk_insert_data('etc', mydb.engine, etc_data)
+
+
+
